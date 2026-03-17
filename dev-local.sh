01bed24425674df78971e0795cde160a2ec31e04
Monika Kubianka <mkubianka@gmail.com>
2026-03-17 20:16:52 +0100

    Fix authorization and routing

---
+++ dev-local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export VITE_API_URL=http://localhost:3000
+$(which node) ./node_modules/.bin/vite --force
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": process.env.VITE_API_URL || "http://localhost:3000",
+      "/admin": process.env.VITE_API_URL || "http://localhost:3000",
     },
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": "http://localhost:3000",
+//     },
+//   },
+// });
